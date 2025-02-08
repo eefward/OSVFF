6cfaf0885c4cf63097ad5793e3bedaeef9fb250e
GuestyTheBesty <guestythebesty841@gmail.com>
2025-02-08 02:29:11 -0800

    app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import render_template, request, redirect
+from flask_session import Session
+
+app = Flask(__name__)
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+
+@app.route('/')
+def home():
+    return render_template("index.html")
